ea8191e2b4e5f662c4c3f5d74c5eb12b196ba68b
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-03 19:25:45 +0300

    Update engex.py

--- engex.py
+++ engex.py
@@ -62,6 +62,85 @@ def obj(row):
 
 df['word'] = df.apply(obj, axis=1)
 
+model_g = api.load('glove-wiki-gigaword-300')
+
+def opt(row):
+    a = set()
+    if row['task'] == 'select_word':
+        try:
+            a.add(row['word'])
+            for i in model_g.similar_by_word(row['word'], topn=2):
+                a.add(i[0].lower())
+
+            return list(a)
+        except:
+            return row['word']
+
+df['options'] = df.apply(opt, axis=1)
+df['options'] = df.apply(lambda x: [] if x['task'] == 'missing_word' else x['options'], axis=1)
+
+def chunk(row):
+    try:
+        p = []
+        a = [] + ['object of preposition', 'direct object']
+        if row['task'] == 'phrases':
+            for chunk in nlp(row['sentences']).noun_chunks:
+                if len(chunk.text) >=7:
+                    p.append(chunk.text)
+                    a.append(spacy.explain(chunk.root.dep_))
+            v = dict(zip(p,a))
+            q = np.random.choice(list(p))
+        return q, list(set(a)), spacy.explain(chunk.root.dep_)
+    except:
+        pass
+
+
+df['answer'] = 0
+
+ddf = df.copy()
+
+for i in range(len(ddf)):
+    if ddf.loc[i, 'task'] == 'phrases':
+        try:
+            q = chunk(ddf.loc[i])
+            ddf.loc[i,'word'] = list(q)[0]
+            ddf.at[i,'options'] = list(q)[1]
+            ddf.loc[i,'answer'] = list(q)[2]
+        except:
+            pass
+
+for i in range(len(ddf)):
+    if (ddf.loc[i, 'task'] == 'select_word' or ddf.loc[i, 'task'] == 'missing_word'):
+        ddf.loc[i,'answer'] = ddf.loc[i, 'word']
+
+def sentgen(row):
+    if row['task'] == 'select_sent':
+        try:
+            d = [token for token in nlp(row['sentences']) if token.pos_ in ['VERB']]
+            x = np.random.choice(d)
+            t = [str(x), x._.inflect('VBP'), x._.inflect('VBG'), x._.inflect('VBD')]
+            s = []
+            if t[0] in (row['sentences'].split(' ')):
+                sent_1 = row['sentences'].replace(t[0], t[1])
+                s.append(sent_1)
+                sent_2 = row['sentences'].replace(t[0], t[2])
+                s.append(sent_2)
+                sent_3 = row['sentences'].replace(t[0], t[3])
+                s.append(sent_3)
+            return row['sentences'], s
+        except:
+            pass
+
+for i in range(len(ddf)):
+    if ddf.loc[i, 'task'] == 'select_sent':
+        try:
+            s = sentgen(ddf.loc[i])
+            ddf.loc[i,'word'] = list(s)[0]
+            ddf.at[i,'options'] = list(s)[1]
+            ddf.loc[i,'answer'] = list(s)[0]
+        except:
+            pass
+
 
 
 
