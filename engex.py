1b1aeb2c49d2ce73d132d0ad934cbee584877bc4
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-04 11:16:47 +0300

    Update engex.py

--- engex.py
+++ engex.py
@@ -24,121 +24,120 @@ text = st.text_area('Input your text.')
 
 
 
-def gen_ex(text):
-    splitter = SentenceSplitter(language='en')
-    sentences = splitter.split(text=text)
-            
-    df = pd.DataFrame()
-            
-    l = []
-    for i in sentences:
-        if i == "":
-            pass
-        else:
-            l.append(i)
-    df['sentences'] = l
+splitter = SentenceSplitter(language='en')
+sentences = splitter.split(text=text)
         
-    df['sentences'] = df['sentences'].apply(lambda x: contractions.fix(x))
+df = pd.DataFrame()
         
-    df.loc[:,'task'] = df.apply(lambda x: np.nan if len(x['sentences'].split())<=7 else np.random.choice(['select_word', 'missing_word', 'phrases', 'select_sent']), axis=1)
+l = []
+for i in sentences:
+    if i == "":
+        pass
+    else:
+        l.append(i)
+df['sentences'] = l
     
-    nlp = en_core_web_sm.load()
+df['sentences'] = df['sentences'].apply(lambda x: contractions.fix(x))
     
-    def obj(row):
-        z = []
-        if (row['task'] == 'select_word' or  row['task'] =='missing_word'):
-            for token in nlp(row['sentences']):
-                if token.pos_ in ['VERB', 'ADJ'] and len(token) > 2:
-                    z.append(str(token).lower())
-            try:
-                return np.random.choice(z)
-            except:
-                pass
-        else:
-            pass
-    
-    df['word'] = df.apply(obj, axis=1)
-    
-    model_g = api.load('glove-wiki-gigaword-300')
+df.loc[:,'task'] = df.apply(lambda x: np.nan if len(x['sentences'].split())<=7 else np.random.choice(['select_word', 'missing_word', 'phrases', 'select_sent']), axis=1)
 
-    def opt(row):
-        a = set()
-        if row['task'] == 'select_word':
-            try:
-                a.add(row['word'])
-                for i in model_g.similar_by_word(row['word'], topn=2):
-                    a.add(i[0].lower())
-    
-                return list(a)
-            except:
-                return row['word']
-    
-    
-    df['options'] = df.apply(opt, axis=1)
-    df['options'] = df.apply(lambda x: [] if x['task'] == 'missing_word' else x['options'], axis=1)
+nlp = en_core_web_sm.load()
 
-    df['answer'] = 0
-
-    def chunk(row):
+def obj(row):
+    z = []
+    if (row['task'] == 'select_word' or  row['task'] =='missing_word'):
+        for token in nlp(row['sentences']):
+            if token.pos_ in ['VERB', 'ADJ'] and len(token) > 2:
+                z.append(str(token).lower())
         try:
-            p = []
-            a = [] + ['object of preposition', 'direct object']
-            if row['task'] == 'phrases':
-                for chunk in nlp(row['sentences']).noun_chunks:
-                    if len(chunk.text) >=7:
-                        p.append(chunk.text)
-                        a.append(spacy.explain(chunk.root.dep_))
-                v = dict(zip(p,a))
-                q = np.random.choice(list(p))
-            return q, list(set(a)), spacy.explain(chunk.root.dep_)
+            return np.random.choice(z)
         except:
             pass
-    
-    for i in range(len(df)):
-        if df.loc[i, 'task'] == 'phrases':
-            try:
-                q = chunk(df.loc[i])
-                df.loc[i,'word'] = list(q)[0]
-                df.at[i,'options'] = list(q)[1]
-                df.loc[i,'answer'] = list(q)[2]
-            except:
-                pass
-    
-    for i in range(len(df)):
-        if (df.loc[i, 'task'] == 'select_word' or df.loc[i, 'task'] == 'missing_word'):
-            df.loc[i,'answer'] = df.loc[i, 'word']
+    else:
+        pass
 
-    def sentgen(row):
-        if row['task'] == 'select_sent':
-            try:
-                d = [token for token in nlp(row['sentences']) if token.pos_ in ['VERB']]
-                x = np.random.choice(d)
-                t = [str(x), x._.inflect('VBP'), x._.inflect('VBG'), x._.inflect('VBD')]
-                s = []
-                if t[0] in (row['sentences'].split(' ')):
-                    sent_1 = row['sentences'].replace(t[0], t[1])
-                    s.append(sent_1)
-                    sent_2 = row['sentences'].replace(t[0], t[2])
-                    s.append(sent_2)
-                    sent_3 = row['sentences'].replace(t[0], t[3])
-                    s.append(sent_3)
-                return row['sentences'], s
-            except:
-                pass
+df['word'] = df.apply(obj, axis=1)
+
+model_g = api.load('glove-wiki-gigaword-300')
+
+def opt(row):
+    a = set()
+    if row['task'] == 'select_word':
+        try:
+            a.add(row['word'])
+            for i in model_g.similar_by_word(row['word'], topn=2):
+                a.add(i[0].lower())
+
+            return list(a)
+        except:
+            return row['word']
+
+
+df['options'] = df.apply(opt, axis=1)
+df['options'] = df.apply(lambda x: [] if x['task'] == 'missing_word' else x['options'], axis=1)
+
+df['answer'] = 0
+
+def chunk(row):
+    try:
+        p = []
+        a = [] + ['object of preposition', 'direct object']
+        if row['task'] == 'phrases':
+            for chunk in nlp(row['sentences']).noun_chunks:
+                if len(chunk.text) >=7:
+                    p.append(chunk.text)
+                    a.append(spacy.explain(chunk.root.dep_))
+            v = dict(zip(p,a))
+            q = np.random.choice(list(p))
+        return q, list(set(a)), spacy.explain(chunk.root.dep_)
+    except:
+        pass
+
+for i in range(len(df)):
+    if df.loc[i, 'task'] == 'phrases':
+        try:
+            q = chunk(df.loc[i])
+            df.loc[i,'word'] = list(q)[0]
+            df.at[i,'options'] = list(q)[1]
+            df.loc[i,'answer'] = list(q)[2]
+        except:
+            pass
+
+for i in range(len(df)):
+    if (df.loc[i, 'task'] == 'select_word' or df.loc[i, 'task'] == 'missing_word'):
+        df.loc[i,'answer'] = df.loc[i, 'word']
+
+def sentgen(row):
+    if row['task'] == 'select_sent':
+        try:
+            d = [token for token in nlp(row['sentences']) if token.pos_ in ['VERB']]
+            x = np.random.choice(d)
+            t = [str(x), x._.inflect('VBP'), x._.inflect('VBG'), x._.inflect('VBD')]
+            s = []
+            if t[0] in (row['sentences'].split(' ')):
+                sent_1 = row['sentences'].replace(t[0], t[1])
+                s.append(sent_1)
+                sent_2 = row['sentences'].replace(t[0], t[2])
+                s.append(sent_2)
+                sent_3 = row['sentences'].replace(t[0], t[3])
+                s.append(sent_3)
+            return row['sentences'], s
+        except:
+            pass
+
+for i in range(len(df)):
+    if df.loc[i, 'task'] == 'select_sent':
+        try:
+            s = sentgen(df.loc[i])
+            df.loc[i,'word'] = list(s)[0]
+            df.at[i,'options'] = list(s)[1]
+            df.loc[i,'answer'] = list(s)[0]
+        except:
+            pass
+df.dropna()
+st.write('Генерация завершена')
     
-    for i in range(len(df)):
-        if df.loc[i, 'task'] == 'select_sent':
-            try:
-                s = sentgen(df.loc[i])
-                df.loc[i,'word'] = list(s)[0]
-                df.at[i,'options'] = list(s)[1]
-                df.loc[i,'answer'] = list(s)[0]
-            except:
-                pass
-    df.dropna()
-    st.write('Генерация завершена')
-    
-submit = st.button('Submit', on_click=gen_ex)
+number = st.input_number(value=int)
 
 #st.subtitle('Генерация завершена.')
 
