04f60892797913d4dfe3a74d46cadfbd30e0fa6b
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-04 12:56:28 +0300

    Update engex.py

--- engex.py
+++ engex.py
@@ -147,7 +147,7 @@ else:
 
 form_2 = st.form("my_2_form")
 num = form_2.number_input('Input num', step=1, max_value=10)
-subm = form.form_submit_button('Submit')
+subm = form_2.form_submit_button('Submit')
 
 
 
