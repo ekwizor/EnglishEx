8f0bf9b832e8f05bc4f86014fa6a768ea4031ad4
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-07 17:18:42 +0300

    Update Pract.py

--- Pract.py
+++ Pract.py
@@ -1,55 +1,39 @@
 import streamlit as st
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from string import punctuation
 import random
 
-def generate_exercises(text):
+def generate_exercises(text, num_exercises):
     # Разделяем текст на предложения
-    sentences = sent_tokenize(text)
-
+    sentences = text.split('. ')
+    selected_sentences = random.sample(sentences, num_exercises)
+    
     exercises = []
-    for sentence in sentences:
-        # Токенизация слов
-        words = word_tokenize(sentence.lower())
-
-        # Удаление стоп-слов и пунктуации
-        words = [word for word in words if word not in stopwords.words("english") and word not in punctuation]
-
-        # Проверка, что предложение содержит достаточно слов
-        if len(words) >= 3:
-            # Выбираем случайное слово из предложения
-            target_word = random.choice(words)
-
-            # Заменяем выбранное слово символами подчеркивания
-            exercise_sentence = sentence.replace(target_word, "______")
-
-            exercises.append((exercise_sentence, target_word))
-
+    for sentence in selected_sentences:
+        # Удаляем знаки препинания
+        sentence = sentence.replace('.', '').replace(',', '').replace('!', '').replace('?', '')
+        # Разделяем предложение на слова
+        words = sentence.split(' ')
+        # Выбираем случайное слово в предложении
+        selected_word = random.choice(words)
+        
+        exercise = f"Замените слово '{selected_word}' в предложении: {sentence}"
+        exercises.append(exercise)
+    
     return exercises
 
-
-# Основной код приложения
 def main():
-    # Заголовок и описание приложения
     st.title("Генератор упражнений по английскому языку")
-    st.write("Введите текст, чтобы сгенерировать упражнения.")
-
-    # Поле для ввода текста
-    user_text = st.text_area("Введите текст")
-
-    # Кнопка для генерации упражнений
+    
+    # Ввод текста пользователем
+    text = st.text_area("Введите текст", "Example sentence 1. Example sentence 2. Example sentence 3.")
+    
+    # Ввод количества упражнений пользователем
+    num_exercises = st.slider("Выберите количество упражнений", 1, 10)
+    
+    # Генерация упражнений по нажатию кнопки
     if st.button("Сгенерировать упражнения"):
-        exercises = generate_exercises(user_text)
-
-        if len(exercises) > 0:
-            st.subheader("Упражнения:")
-            for exercise in exercises:
-                st.write("- " + exercise[0])
-                st.write("Ответ: " + exercise[1])
-
-        else:
-            st.write("Нет подходящих предложений для генерации упражнений.")
+        exercises = generate_exercises(text, num_exercises)
+        for exercise in exercises:
+            st.write(exercise)
 
 if __name__ == "__main__":
     main()
