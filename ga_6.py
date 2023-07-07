3aa9298ca5a98189e87d63dfca7250f23cd00618
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-07 13:09:57 +0300

    Update ga_6.py

--- ga_6.py
+++ ga_6.py
@@ -182,9 +182,6 @@ def gen_ex(text, num):
 
 
 if __name__ == '__main__':
-        st.title('Генератор упражнений по английскому')
-        text = st.text_area('Input your text.')
-        num = st.number_input('Input num of exercises', step=1, max_value=10)
         st.button("Submit", on_click=gen_ex, args=(text, num))
 
     
