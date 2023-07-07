2972d92b058d571082eb009f681d10597b9ed5c6
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-07 16:32:51 +0300

    Update ga_6.py

--- ga_6.py
+++ ga_6.py
@@ -196,7 +196,8 @@ if __name__ == '__main__':
                 if 'dataframe' not in st.session_state:
                         st.session_state['dataframe'] = gen_ex(text, num)
 
-        data = st.session_state['dataframe'][0]
+        df = st.session_state['dataframe'][0]
+        data=df.sample(num, ignore_index=True)
 
         st.write(data)
       
