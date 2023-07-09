9ef59ccf0742dc48f40849e619e165de2c3ff30f
Roman Shabrov <126798126+ekwizor@users.noreply.github.com>
2023-07-09 12:55:32 +0300

    Update Pract.py

--- Pract.py
+++ Pract.py
@@ -185,7 +185,7 @@ def main(text, num):
                 st.write(f'Варианты ответов: {option_str}')
     
                 st.write(answ)
-                user_answer = st.selectbox(f'Выберите правильный ответ {i+1}:', [*option if answ not in *option else *option, answ] , key=f'phrase_{i}')
+                user_answer = st.selectbox(f'Выберите правильный ответ {i+1}:', [*option if answ not in option else *option, answ] , key=f'phrase_{i}')
                 check_button = st.button(f'Проверить {i+1}')
     
                 if check_button:
